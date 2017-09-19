afa590940debcc700a4d288acf71ba37ec843d1f
dabao <oubl2343@gmail.com>
2017-09-19 10:10:31 +0800

    调整了阶段配置样式，添加js插件select2用于制作通知人员select框。todo：js效果添加其他标签页。

--- pms-web/src/main/webapp/statics/js/projectManage/projectTemple.js
+++ pms-web/src/main/webapp/statics/js/projectManage/projectTemple.js
@@ -1,22 +1,22 @@
+$(document).ready(function() {
+    $('.js-example-basic-multiple').select2();
+});
+
 var templeForm = {
     template: ""+
     "               <div class=\"form-group row\">\n" +
-    "                <label class=\"col-sm-2 col-form-label\">模板名称：</label>\n" +
+    "                <label class=\"col-sm-2 col-form-label\">默认完成时间：上一环节偏移</label>\n" +
     "                <div class=\"col-sm-10\">\n" +
     "                    <input type=\"text\" class=\"form-control\" placeholder=\"输入模板名称\">\n" +
     "                </div>\n" +
     "            </div>"
-};
-var Child = {
-    template: '<h1>自定义组件!</h1>'
 };
 
 var vm = new Vue({
     el: '#dpTemple',
     data: {},
     components: {
-        "tf": templeForm,
-        "Child":Child
+        "temple-form": templeForm,
     },
     methods: {}
 })
